a492ea1b0737e4875310e55a387421c3ad92855d
JosielEletricista <carlos.filho1@escolar.ifrn.edu.br>
2024-08-20 14:33:15 -0300

    Camera e Pulo

--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -3,21 +3,52 @@ using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+using System;
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
+    public int forcaPulo = 7;
     Rigidbody rb;
+    public bool noChao;
+    
     void Start()
     {
+
         TryGetComponent(out rb);
+
     }
 
-    // Update is called once per frame
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (!noChao && collision.gameObject.tag == "Chão")
+            {
+                noChao = true;
+            }
+        }
+
     void Update()
     {
+       
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        Vector3 direcao = new Vector3(h, 0, v);
+
+        Vector3 direcao = new Vector3(x:h, y:0, z:v);
         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+
+
+         if (Input.GetKeyDown(KeyCode.Space) && noChao)
+         {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+         }
+
+                if(transform.position.y <= -7)
+                 {
+
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+                 }
     }
 }
