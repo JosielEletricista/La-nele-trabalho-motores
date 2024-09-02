f524b4669aa2481c3e67dfc8d357b05fe3ec0146
JosielEletricista <carlos.filho1@escolar.ifrn.edu.br>
2024-09-02 16:19:26 -0300

    Camera smooth

--- Meu Game 3D/Assets/Scripts/CameraBall.cs
+++ Meu Game 3D/Assets/Scripts/CameraBall.cs
@@ -6,16 +6,18 @@ public class CameraBall : MonoBehaviour
 {
     private Transform alvo;
     public Vector3 offset;
+    public int suavidade = 5;
     // Start is called before the first frame update
     void Start()
     {
         alvo = GameObject.FindWithTag("Player").transform;
-        offset = alvo.position - transform.position;
+        offset = transform.position - alvo.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = alvo.position - offset;
+        Vector3 posFinal = alvo.position + offset;
+        transform.position = Vector3.Lerp(a:transform.position, b:posFinal, t:suavidade * Time.deltaTime);
     }
 }
